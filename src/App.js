9c702ee5f89324d42be86f2181cc023f19491e76
ghazibenrhouma <ghazybenrhouma@gmail.com>
2023-01-04 10:22:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ghazi Ben Rhouma</h1>
+      <h3>hello world </h3>
+    </div>
+  );
+}
+
+export default App;
